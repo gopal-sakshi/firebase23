725c0f4695c3aea18e7067ec459728a8ea4bc83c
gopal-sakshi <gopal612@gmail.com>
2022-07-07 14:43:16 +0530

    added comments on how to use firebase23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,21 @@ export class AppComponent implements OnInit{
     console.log(name23);
   }
 
+  /**
+   *  HOW TO USE
+   * 
+   * simply run the angular application -----> ng serve --port 4343
+   * ensure notifications are allowed in your browser
+   * then check for console.logs ----> you'll see a token printed 
+   *    from this line ------------------> fire-message23.service.ts:27 
+   * 
+   * copy this token... open postman... backEndQueries... misc... sendNotification
+   *  edit the body object with this token
+   *  just for timepass ---> copy paste Authorization headers too (bcoz, it only worked after I re-entered the same authorization token)
+   *  you can see Authorization in this link
+   * 
+   * https://console.firebase.google.com/u/2/project/gopal61288-01/settings/cloudmessaging/web:ZjdjNTIwN2UtNTc2Yi00NzdmLWE5M2QtZDE4Y2YzNDAwZmMz
+   * 
+   */
+
 }
