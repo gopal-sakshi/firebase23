425ad3bf7c17cecf3f00c4c365317a51e1f7adfc
gopal612 <gopal612@gmail.com>
2024-10-09 15:57:50 +0530

    revision23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,29 +18,4 @@ export class AppComponent implements OnInit{
     // this.fireMessage23.listen();ss
   }
 
-  /**
-   *  HOW TO USE
-   * 
-   * simply run the angular application -----> ng serve --port 4343
-   * ensure notifications are allowed in your browser
-   * then check for console.logs ----> you'll see a token printed 
-   *    from this line ------------------> fire-message23.service.ts:27 
-   * 
-   * copy this token... open postman... backEndQueries... misc... sendNotification
-   *  edit the body object with this token
-   *  just for timepass ---> copy paste Authorization headers too (bcoz, it only worked after I re-entered the same authorization token)
-   *  you can see Authorization in this link
-   * 
-   * https://console.firebase.google.com/u/2/project/gopal61288-01/settings/cloudmessaging/web:ZjdjNTIwN2UtNTc2Yi00NzdmLWE5M2QtZDE4Y2YzNDAwZmMz
-   * 
-   * 
-   * it seems only for push notifications of fcm, we need these
-   * - firebase-messaging-sw.js
-   * - manifest.json
-   * - add these in angualr.json assets array
-   * 
-   * But if you want to use firebase as database (not push notification)
-   * - then, it seems, we do not need these...
-   */
-
 }
